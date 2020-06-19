51dd0219d87bf900c77fea4370aa6a6e7a5fa962
Stevenzzz1996 <2499143041@qq.com>
2020-06-19 22:11:04 +0800

    调整README文件和文件格式转换文件

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Human-detection
 
-### 项目介绍
+### Introduction:
 
 - 以人为中心的视频分析技术构建
 
@@ -10,9 +10,30 @@
 
 ---
 
-### 项目技术介绍：
+### simple & strong & faster：
 
-- 正在进行中.....
+- YOLOV3为baseline
+- 参考文章：V4、Centernet、DetectoRS、coco比赛何凯明全家桶、Deformable Convolution、Hourglass network
+- 加入DIOU loss(听说比较好用)
+- 感受野模块ASPP+Self_Attention
+- 更换激活函数（mish serise）
+- 加入分层预测计划（参见英伟达语义分割最新工作）
+
+---
+
+### 论坛：
+
+- 1.知乎yunyang关于yolov3实现以及 tf2.0 code
+
+​      地址：
+
+- 2.参见知乎各种基于IOU的loss，一定要看懂原理和实测
+
+​      地址：
+
+以上内容个人理解，可尽情添加。
+
+---
 
 
 
--- model/odgt2txt.py
+++ model/odgt2txt.py
@@ -1,30 +1,45 @@
 import json
 import numpy as np
-
-with open('D:/BaiduNetdiskDownload/personreid/annotation_train.odgt', 'r+') as f: #input filename
+import cv2
+odgt_path = 'path.odgt'# odgt文件存放地址
+with open(odgt_path, 'r+') as f: #input filename
     datalist = f.readlines()
 
 inputfile = []
 inner = {}
 anchor = str()
+def change_data(x_min,x_max,y_min,y_max,image_shape):
+    h,w,_ = image_shape
+    x_min = max(0,x_min)
+    x_max = min(w-1,x_max)
+    y_min = max(0,y_min)
+    y_max = min(h-1,y_max)
+    return x_min,x_max,y_min,y_max
+
+
 for i in np.arange(len(datalist)):
     anchor = str()
     adata = json.loads(datalist[i])
-    image_path ='D:/BaiduNetdiskDownload/personreid/train/'+str(adata['ID'])+ '.jpg'
+    image_path ='D:/BaiduNetdiskDownload/person_train/'+str(adata['ID'])+ '.jpg'
+    read_path = 'D:/BaiduNetdiskDownload/personreid/train/'+str(adata['ID'])+ '.jpg'
     #print(image_path)
     gtboxes = adata['gtboxes']
     # vbox = gtboxes['vbox']
     print(len(gtboxes))
+    src = cv2.imread(read_path)
+    print(src.shape)
     for gtbox in gtboxes:  # Change lines 13 to 27 based on the number of classes you have and their names
         if gtbox['tag'] == 'person':  # here, person = the class name
                 xmin,ymin,w,h = gtbox['vbox']
                 xmax = xmin + w
                 ymax = ymin+ h
-                box = str(' ')+str(xmin)+str(',')+str(ymin)+str(',')+str(xmax)+str(',')+str(ymax)+str(',')+str(0)
+                xmin,xmax,ymin,ymax = change_data(xmin,xmax,ymin,ymax,src.shape)
+                box = " " + ",".join(map(str, [xmin, ymin, xmax, ymax, 1]))
+                # box = str(' ')+str(xmin)+str(',')+str(ymin)+str(',')+str(xmax)+str(',')+str(ymax)+str(',')+str(0)
                 anchor+=box
     print(image_path+anchor)
     src = image_path + anchor
-    with open('person.txt', 'a') as file_handle:  # .txt可以不自己新建,代码会自动新建
+    with open('person_train.txt', 'a') as file_handle:  # .txt可以不自己新建,代码会自动新建
         file_handle.write(src)  # 写入
         file_handle.write('\n')
 
