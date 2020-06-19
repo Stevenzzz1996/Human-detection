905d6223eb8564e9f167dcecbd69412c144a4cce
qam <174.com>
2020-06-19 15:14:10 +0800

    change odgt2txt.py for clear

--- model/odgt2txt.py
+++ model/odgt2txt.py
@@ -1,32 +1,31 @@
 import json
 import numpy as np
+from tqdm import tqdm
 
-with open('D:/BaiduNetdiskDownload/personreid/annotation_train.odgt', 'r+') as f: #input filename
+with open('annotation_train.odgt', 'r+') as f: #input filename
     datalist = f.readlines()
 
-inputfile = []
-inner = {}
-anchor = str()
-for i in np.arange(len(datalist)):
-    anchor = str()
-    adata = json.loads(datalist[i])
-    image_path ='D:/BaiduNetdiskDownload/personreid/train/'+str(adata['ID'])+ '.jpg'
-    #print(image_path)
+
+for data in tqdm(datalist):
+    anchor = ""
+    
+    adata = json.loads(data)
+    
+    image_path ='D:/BaiduNetdiskDownload/personreid/train/'+ adata['ID'] + '.jpg'
+  
     gtboxes = adata['gtboxes']
-    # vbox = gtboxes['vbox']
-    print(len(gtboxes))
-    for gtbox in gtboxes:  # Change lines 13 to 27 based on the number of classes you have and their names
+  
+    for gtbox in gtboxes:  
         if gtbox['tag'] == 'person':  # here, person = the class name
                 xmin,ymin,w,h = gtbox['vbox']
                 xmax = xmin + w
                 ymax = ymin+ h
-                box = str(' ')+str(xmin)+str(',')+str(ymin)+str(',')+str(xmax)+str(',')+str(ymax)+str(',')+str(0)
-                anchor+=box
-    print(image_path+anchor)
+                box = " " + ",".join(map(str, [xmin, ymin, xmax, ymax, 1]))
+                anchor += box
+
     src = image_path + anchor
     with open('person.txt', 'a') as file_handle:  # .txt可以不自己新建,代码会自动新建
-        file_handle.write(src)  # 写入
-        file_handle.write('\n')
+        file_handle.write(src + '\n')  # 写入
 
 
 
